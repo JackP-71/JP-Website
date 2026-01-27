f3c44b5aee323fbac14fd9358435d5cd3c8fc87b
JackP-71 <jacktompercival@gmail.com>
2026-01-27 18:18:52 +0000

    Added javascript to website
    a footer has been added using javascript - any other js to be written will be written in the script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+//code to automatically write the footer - if more pages are created, this will be referenced throughout
+
+document.querySelector(".writeFooter").innerHTML = `
+    <footer>
+    <center>
+        <p>© Jack Percival - 2026</p> 
+    </center>
+    </footer>
+`;
