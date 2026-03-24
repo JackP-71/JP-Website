e899dbe3df3f25c828b30c59b852a6fe4ee7a5a8
Jack <jacktompercival@gmail.com>
2026-03-24 18:52:40 +0000

    NEW VERSION OF SITE

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//code to automatically write the footer - if more pages are created, this will be referenced throughout
+//code to automatically write the footer and header - if more pages are created, this will be referenced throughout
 
-document.querySelector(".writeFooter").innerHTML = `
-    <footer>
-    <center>
-        <p>© Jack Percival - 2026</p> 
-    </center>
-    </footer>
+document.querySelector(".writeHeader").innerHTML = `
+        <div id="header">
+            <a href="../index.html" style="text-decoration: none;"><h1>> Jack Percival.<h1></a>
+        </div>
+`;
+
+document.querySelector(".writeNavbar").innerHTML = `
+    <div id="navbar">
+        <p><a href="index.html">Return</a> | <a href="projects.html">Projects</a></p>
+    </div>
+`;
+
+document.querySelector(".writeFooter").innerHTML = `              
+        <footer id="footer" style="margin-top: 10px;">
+            <p>© Jack Percival - 2026.</p>
+        </footer>
 `;
