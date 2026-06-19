fc5334da06db79e8a7ff5b49feef439d41795360
Jack <jacktompercival@gmail.com>
2026-06-19 14:20:07 +0100

    complete overhaul of the site ^^

--- script.js
+++ script.js
@@ -2,18 +2,60 @@
 
 document.querySelector(".writeHeader").innerHTML = `
         <div id="header">
-            <a href="../index.html" style="text-decoration: none;"><h1>> Jack Percival.<h1></a>
+            <a href="../index.html" style="text-decoration: none;"><h1>> a simple portfolio.<h1></a>
         </div>
 `;
 
 document.querySelector(".writeNavbar").innerHTML = `
     <div id="navbar">
-        <p><a href="index.html">Return</a> | <a href="projects.html">Projects</a></p>
+        <p>| <a href="index.html">About</a> | <a href="projects.html">Programming</a> |</p>
     </div>
 `;
 
 document.querySelector(".writeFooter").innerHTML = `              
         <footer id="footer" style="margin-top: 10px;">
-            <p>© Jack Percival - 2026.</p>
+            <p>© 2026.</p>
         </footer>
+        <p></p>
+        <center>
+        <div class="box" style="max-width:fit-content; background-color: var(--bg-color);">
+            <div class="theme-switch-wrapper">
+                <em>Theme Select:⠀</em>
+                <!--
+                <label class="theme-switch" for="checkbox">
+                    <input type="checkbox" id="checkbox" />
+                    <div class="slider round"></div>
+                </label>-->
+                <select id="theme-select">
+                    <option value="gray">Grayscale,</option>
+                    <option value="light">Grayscale - Light,</option>
+                    <option value="purple">Purple,</option> 
+                    <option value="orange">Orange,</option>
+                    <option value="red">Red,</option> 
+                    <option value="blue">Blue,</option> 
+                    <!--<option value="custom">Custom,</option>--> 
+                </select>
+                <!--<div id="themeSelector">
+                    <label for="primaryColor">Primary Color:</label>
+                    <input type="color" id="primaryColor" name="primaryColor" value=#888888>-->
+                </div>
+            </div>
+        </div>
+        </center>
 `;
+
+// scripts for storing the theme
+const themeSelect = document.getElementById("theme-select");
+
+// Apply theme on change
+themeSelect.addEventListener("change", function () {
+    const theme = this.value;
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+});
+// Load saved theme
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+    themeSelect.value = savedTheme;
+}
